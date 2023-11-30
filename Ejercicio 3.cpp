2f3acf7891ea2f32e48628a128f583c011632262
Armenteros05 <sarmerod@myuax.com>
2023-11-30 16:01:23 +0100

    ejercicio 3

--- Ejercicio 3.cpp
+++ Ejercicio 3.cpp
@@ -1,4 +1,24 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
+using namespace std;
+int main (){
+    vector<int> numeros = {50, 20, 60, 10, 30, 40};
+    sort(numeros.begin(), numeros.end());
 
+    cout << "Elementos ordenados:" << endl;
+    for(int num : numeros) {
+        cout << num << " ";
+    }
+    cout << endl;
+
+    auto maxElement = max_element(numeros.begin(), numeros.end());
+    if (maxElement != numeros.end()) {
+        int maxNum = *maxElement;
+        cout << "Numero maximo: " << maxNum << endl;
+    } else {
+        cout << "El vector esta vacio." << endl;
+    }
+
+    return 0;
+}
