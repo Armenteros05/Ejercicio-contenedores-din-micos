897f8ceacf307155d6e6c5bab1c164256f34b015
Armenteros05 <sarmerod@myuax.com>
2023-11-30 15:57:29 +0100

    Ejercicio 2.cpp

--- Ejercicio 2.cpp
+++ Ejercicio 2.cpp
@@ -4,5 +4,26 @@
 
 using namespace std;
 
-int main() {}
+int main() {
+    list<float> lista = {1.1, 2.2, 3.3, 4.4, 5.5};
+    stack<float, list<float>> pila(lista);
+
+    cout << "Contenido inicial de la pila:" << endl;
+    while (!pila.empty()) {
+        cout << pila.top() << endl;
+        pila.pop();
+    }
+
+    pila.push(6.6);
+    pila.push(7.7);
+
+    cout << "\nContenido de la pila despues de push:" << endl;
+    while (!pila.empty()) {
+        cout << pila.top() << endl;
+        pila.pop();
+    }
+
+    return 0;
+
+}
 
