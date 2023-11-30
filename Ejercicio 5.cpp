a5a4b7e910d0570e0787a69e716d25974d5da0e1
Armenteros05 <sarmerod@myuax.com>
2023-11-30 16:07:21 +0100

    ejercicio 5 finalizado

--- Ejercicio 5.cpp
+++ Ejercicio 5.cpp
@@ -2,3 +2,20 @@
 #include <vector>
 #include <algorithm>
 #include <memory>
+using namespace std;
+int main() {
+    auto cuadrado = [](int num) { return num * num; };
+
+    cout << "Cuadrado de 5: " << cuadrado(5) << endl;
+
+    vector<int> numeros = {1, 2, 3, 4, 5};
+    for_each(numeros.begin(), numeros.end(), [](int num) {
+        cout << num << " ";
+    });
+    cout << endl;
+
+    shared_ptr<int> ptr = make_shared<int>(10);
+    cout << "Valor apuntado por ptr: " << *ptr << endl;
+
+    return 0;
+}
