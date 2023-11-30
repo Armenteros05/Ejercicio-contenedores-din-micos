372af94b317adbda31beaae8e53bc2c2dbd40617
Armenteros05 <sarmerod@myuax.com>
2023-11-30 16:03:39 +0100

    .

--- Ejercicio 4.cpp
+++ Ejercicio 4.cpp
@@ -1,4 +1,21 @@
 #include <iostream>
 #include <valarray>
 using namespace std;
-int main() {}
+int main() {
+    valarray<double> valores = {1.0, 2.0, 3.0, 4.0, 5.0};
+
+    cout << "Valores originales: ";
+    for (double valor : valores) {
+        cout << valor << " ";
+    }
+    cout << endl;
+
+    cout << "Suma: " << (valores.sum()) << endl;
+    valores -= 2.0;
+    cout << "Valores - 2.0: ";
+    for (double valor : valores) {
+        cout << valor << " ";
+    }
+    cout << endl;
+
+}
