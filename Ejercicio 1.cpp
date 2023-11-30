4ae39b574e047cd6f951361c3c7c7f4acf8465d8
Armenteros05 <sarmerod@myuax.com>
2023-11-30 15:54:18 +0100

    ejercicio 1

--- Ejercicio 1.cpp
+++ Ejercicio 1.cpp
@@ -3,4 +3,15 @@
 using namespace std;
 
 int main() {
+    vector<int> numeros = {10, 20, 30, 40, 50};
+
+    numeros.push_back(60);
+    numeros.push_back(70);
+    numeros.push_back(80);
+    cout << "Elementos del vector:" << endl;
+    for (vector<int>::iterator it = numeros.begin(); it != numeros.end(); ++it) {
+        cout << *it << endl;
+    }
+
+    return 0;
 }
